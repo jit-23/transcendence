6a0efce450fa6fdf39a2c30d520bc0914d9351d9
paulorsfaria <paulofaria.esaf@gmail.com>
2026-04-07 16:55:17 +0100

    starting 42auth

--- backend/app/src/types/global.d.ts
+++ backend/app/src/types/global.d.ts
@@ -1,14 +1,5 @@
 declare module "qrcode";
 declare module "speakeasy";
-// this file was just made so when compiling error would stop showing up
-// what it basically does is say the compiler this exists stop saying it doesn't
-
-
-
-//    speakeasy and  qrcode where added to package.json and package-lock.json for the 2-factor authentication
-//router.post('/login2FA', login2FA) was added to userRouter for the same reason
-//qr code was added to create account and will show up after successfully creating an account
-// and login was changed to be able to handle 2-factor authentication
-//	twoFactorEnabled twoFactorSecret was added to prisma and verify2FA function was added to userController
-//
+// this file was just made so when compiling a error would stop showing up
+// what it basically does is say to the compiler this exists so please stop saying it doesn't
 
